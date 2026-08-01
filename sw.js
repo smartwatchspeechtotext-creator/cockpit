44167774c7e9ecfed36b460fce0cf7eb31226b56
smartwatchspeechtotext-creator <smartwatchspeechtotext@gmail.com>
2026-08-01 18:21:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    jedes Gerät die neue Fassung.
    ===================================================================== */
 
-const STAND = 'cockpit-huelle-v8';
+const STAND = 'cockpit-huelle-v9';
 
 const DATEIEN = [
   './',
